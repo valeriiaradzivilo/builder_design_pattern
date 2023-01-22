1c6018a4766bad98d6c24dced495061af3d29a99
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2023-01-22 19:59:07 +0200

    finished version ready to be added to portfolio

---
+++ lib/sushi/sushi_all.dart
@@ -0,0 +1,39 @@
+import 'package:builder_design_pattern/ingredients/ingredients_all.dart';
+import 'package:builder_design_pattern/sushi/sushi_class.dart';
+
+class CookedTunaAndAvocadoHRoll extends Sushi{
+  CookedTunaAndAvocadoHRoll()
+  {
+    name = "Cooked Tuna and Avocado H/Roll";
+    ingredients=[Rice(),Avocado(),Egg(),Tuna(),Sesame()];
+    fullPrice = setPrice();
+  }
+}
+
+class SalmonAndAvocadoHRoll extends Sushi{
+  SalmonAndAvocadoHRoll()
+  {
+    name = "Salmon and Avocado H/Roll";
+    ingredients=[Rice(),Avocado(),Egg(),Salmon(),Sesame()];
+    fullPrice = setPrice();
+  }
+}
+
+class VegetarianHRoll extends Sushi{
+  VegetarianHRoll()
+  {
+    name = "Vegetarian H/Roll";
+    ingredients=[Rice(),Sesame()];
+    fullPrice = setPrice();
+  }
+}
+
+class CrispyChickenHRoll extends Sushi{
+  CrispyChickenHRoll()
+  {
+    name = "Crispy Chicken H/Roll";
+    ingredients=[Rice(),Crisp(),Egg(),Sesame()];
+    fullPrice = setPrice();
+  }
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:builder_design_pattern/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const BuilderApp());
+    await tester.pumpWidget(const BuilderApp(sushiEdit: null,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
